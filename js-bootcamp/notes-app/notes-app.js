1fa415090a393d555a8350ab19eac9af067f752c
Elvis Olivares <elvisoe@gmail.com>
2020-01-12 19:16:33 -0500

    j-07-10-10_rendering_our_filtered_data

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -9,18 +9,25 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-// const filters = {
-//     searchText: ''
-// }
+const filters = {
+    searchText: ''
+}
 
-// const renderNotes =  function (notes, filters){
-//     const filteredNotes = notes.filter(function (note) {
-//         return note.title.toLowerCase().includes(filters.searchTex.toLowerCase())    
-//     })
-//         console.log(filteredNotes)
-// }
+const renderNotes =  function (notes, filters){
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())    
+    })
 
-//  renderNotes(notes, filters)
+        document.querySelector('#notes').innerHTML=''
+    
+        filteredNotes.forEach(function (note, filters){
+            const noteEl = document.createElement('p')
+            noteEl.textContent = note.title
+            document.querySelector('#notes').appendChild(noteEl)
+        })
+}
+
+ renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function(e){ 
     e.target.textContent = 'The button was clicked'
@@ -33,9 +40,8 @@ document.querySelector('#remove-all').addEventListener('click', function(){
 })
 
 document.querySelector("#search-text").addEventListener('input', function (e){
-    console.log(e.target.value)
-    //filters.searchText = e.target.value
-    //renderNotes(notes, filters)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
 
